098ef5448b3502b498dd0cd15254a908f1f0f5a3
PauloVictorGomes <paulo_victornense@hotmail.com>
2020-08-29 17:28:26 -0300

    required

---
+++ js/calcula.js
@@ -0,0 +1,19 @@
+function getMensagemMenu(){
+    var op = ""
+    op = prompt("Entre com a operação: \n(+) soma \n(-) subtrai \n(*)mutiplica \n(/) divide \n(%) resto da divisão")
+    return op
+}
+function calcula(operador, operando1, operando2){
+    var tot = 0
+    switch (op){
+        case "+":
+
+            break
+        case "-":
+
+            break
+        case "*":
+
+            break
+    }
+}
